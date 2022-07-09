fb16a878c5c9b8d12d4ae9d51d46b50d5fe464cf
Rodrigo Longhi <rodrigo.longhi@mercadolivre.com>
2022-07-08 22:20:35 -0300

    initial structure

---
+++ philo/src/philosophers.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rfelipe- <rfelipe-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/08 22:13:56 by rfelipe-          #+#    #+#             */
+/*   Updated: 2022/07/08 22:18:24 by rfelipe-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <../includes/philosophers.h>
+
+int	main(int argc, char **argv)
+{
+	if (argc != 0 && argv)
+		printf("Hello philosophers!\n");
+	return (0);
+}
